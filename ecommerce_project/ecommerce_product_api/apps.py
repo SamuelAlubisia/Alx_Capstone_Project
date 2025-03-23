dc58092d480bdf4d4accf2564f509ab8f0c296bb
SamuelAlubisia <salubisia@gmail.com>
2025-03-23 23:01:54 +0300

    Created models and made migrations

---
+++ ecommerce_project/ecommerce_product_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EcommerceProductApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ecommerce_product_api'
---
+++ ecommerce_project/ecommerce_product_api/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    image_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.category.name}"
+
+    
